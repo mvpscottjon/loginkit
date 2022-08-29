599870a558c3ebf9393f368b394ee406fb160c16
Seven Tsai <mvpscottjon@gmail.com>
2022-08-29 10:24:56 +0800

    add base files

---
+++ Sources/LoginKit/LoginKit.swift
@@ -0,0 +1,23 @@
+public struct LoginKit {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+    
+    public func login(completion: @escaping (Bool, Error?) -> Void) {
+        
+    }
+
+    public func logout(completion: @escaping (Bool, Error?) -> Void) {
+        
+    }
+    
+    public func signup(completion: @escaping (Bool, Error?) -> Void) {
+        
+    }
+    
+    public func deleteAccount(completion: @escaping (Bool, Error?) -> Void) {
+        
+    }
+    
+}
